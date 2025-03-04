d0f09ad67a323e0b238f3f3dc7644cbab0d05047
nguyen-duy06 <ghdghiggjjh@gmail.com>
2025-03-04 21:19:18 +0700

    Add files via upload

---
+++ input_output/B2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    printf("nhap cac tham so vao he phuong trinh sau:\n");
+    printf("ax + by = c\n");
+    printf("dx + ey = f\n");
+    double a,b,c,d,e,f;
+    scanf("%lf %lf %lf %lf %lf %lf",&a,&b,&c,&d,&e,&f);
+    double y = ((a*f - c*d)/(a*e - b*d));
+    double x = ((c - b*y) / a);
+    printf("nghiem x  = %lf\n",x);
+    printf("nghiem y  = %lf\n",y);
+    return 0;
+}
