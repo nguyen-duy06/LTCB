d265fc0ebca4aeb1c1712014741f796883d22995
nguyen-duy06 <ghdghiggjjh@gmail.com>
2025-03-08 16:33:30 +0700

    Update B1.c

--- input_output/B1.c
+++ input_output/B1.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main() {
     int x;
+    printf("nhap vao 1 so nguyen x: \n");
     scanf("%d",&x);
-    printf("%d\n%d\n%d\n",x*x,x*x*x,x*x*x*x);
+    printf(" x^2: %d\n x^3: %d\n x^4: %d\n",x*x,x*x*x,x*x*x*x);
     return 0;
 }
