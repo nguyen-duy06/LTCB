feec805e6f20c4a0e9c972e579e1b53654f21f0c
nguyen-duy06 <ghdghiggjjh@gmail.com>
2025-03-08 16:35:01 +0700

    Update B2.c

--- input_output/B2.c
+++ input_output/B2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main() {
     int ngay,thang,nam;
+    printf("nhap ngay thang nam bat ki: ");
     scanf("%d%d%d",&ngay,&thang,&nam);
     printf("hom nay la ngay %d/%d/%d",ngay,thang,nam);
     return 0;
