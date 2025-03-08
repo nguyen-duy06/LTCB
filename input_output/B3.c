a1f2df18528e68d69ae2db268e0893c4bcbc557a
nguyen-duy06 <ghdghiggjjh@gmail.com>
2025-03-08 16:38:38 +0700

    Update B3.c

--- input_output/B3.c
+++ input_output/B3.c
@@ -2,11 +2,12 @@
 
 int main() {
     int x,y;
+    printf("nhap vao 2 so nguyen  bat ki: ");
     scanf("%d%d",&x,&y);
-    printf("ket qua phep cong:%d\nphep tru:%d\nphep nhan:%d\nphep chia:%d\n",x+y,x-y,x*y,x/y);
+    printf("ket qua\n phep cong:%d\n phep tru:%d\n phep nhan:%d\n phep chia:%d\n",x+y,x-y,x*y,x/y);
     if (x % y)
-        printf("phep chia co du\n");
+        printf("*phep chia co du\n");
     else
-        printf("phep chia het");
+        printf("*phep chia het");
     return 0;
 }
