331d1fce911af715af091563e166543b314d66e5
nguyen-duy06 <ghdghiggjjh@gmail.com>
2025-03-22 15:23:40 +0700

    Update B7.c

--- do_while/B7.c
+++ do_while/B7.c
@@ -1,17 +1,18 @@
 #include<stdio.h>
-double sum(int n)
+double sum(long long n)
 {
     double sum = 0.0;
-    for (int i = 1; i <= n; ++i)
+    for (long long i = 1; i <= n; ++i)
     {
         sum += (1.0/i);
     }
     return sum;
 }
 int main()
-{   int n;
+{   
+    long long n;
     printf("nhap vao mot gia tri n: ");
-    scanf("%d",&n);
-    printf(" 1 + 1/2 + ... + 1/n = %lf",sum(n));
+    scanf("%lld",&n);
+    printf(" 1 + 1/2 + ... + 1/n = %llf",sum(n));
     return 0;
 }
